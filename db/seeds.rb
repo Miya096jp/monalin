daa12b6dd969b10ceb86c10f791325dbd3b0ea40
Miya096jp <miyawaki.yske@gmail.com>
2026-03-17 13:29:25 +0900

    Chore: UI作成用のseedsを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "データベースをクリーンアップ中..."
+User.destroy_all
+
+puts "ユーザーを作成中..."
+
+user = User.create!(
+  username: "test_user",
+  email: "test@example.com",
+)
+
+sessions = 3.times.map do |i|
+  user.sessions.create!(session_title: "セッション#{i}")
+end
+
+sessions.each do |s|
+  3.times do |i|
+    s.messages.create!(role: :user, body: "ユーザーメッセージ#{i + 1}")
+    s.messages.create!(role: :ai, body: "AIのメッセージ#{i + 1}")
+  end
+end
+
+puts "seeds作成完了"
