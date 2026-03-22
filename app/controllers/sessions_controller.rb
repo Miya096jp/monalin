e269a11889b21b2c2331226cf0819cdfaadfe063
Miya096jp <miyawaki.yske@gmail.com>
2026-03-22 18:41:54 +0900

    Feature: Turbo frameでセッションリストに編集・削除機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,37 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  before_action :set_session, only: [ :show, :edit, :update, :destroy ]
 
   def show
-    @session = current_user.sessions.find(params[:id])
     @session_messages = @session.messages.order(created_at: :asc)
   end
 
-  def create
-  end
-
   def edit
+    render partial: "sessions/session_edit_row", locals: { session: @session }
   end
 
   def update
+    if @session.update(session_params)
+      render partial: "sessions/session_list_item", locals: { session: @session }
+    else
+      render partial: "sessions/session_edit_row", locals: { session: @session }, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @session.destroy
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@session) }
+      format.html { redirect_to root_path }
+    end
+  end
+
+  private
+
+  def set_session
+    @session = current_user.sessions.find(params[:id])
+  end
+
+  def session_params
+    params.require(:session).permit(:session_title)
   end
 end
