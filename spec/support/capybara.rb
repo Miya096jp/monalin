f85daec00aea0c4b63d2682efd59eecd7f3b675a
Miya096jp <miyawaki.yske@gmail.com>
2026-03-28 11:48:44 +0900

    Config: localでのテスト設定にscreensizeを追加する修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,6 +4,7 @@ require "selenium-webdriver"
 RSpec.configure do |config|
   config.before(:each, type: :system) do |example|
     screen_size = example.metadata[:desktop] ? [ 1280, 800 ] : [ 375, 812 ]
+
     if ENV["SELENIUM_REMOTE_URL"].present?
       driven_by :selenium, using: :headless_chrome, screen_size: screen_size, options: {
         browser: :remote,
@@ -14,7 +15,7 @@ RSpec.configure do |config|
       Capybara.server_port = ENV.fetch("TEST_APP_PORT", 3001).to_i
       Capybara.app_host = "http://#{ENV.fetch("TEST_APP_HOST", "web")}:#{Capybara.server_port}"
     else
-      driven_by :selenium, using: :headless_chrome
+      driven_by :selenium, using: :headless_chrome, screen_size: screen_size
     end
     ActionController::Base.allow_forgery_protection = true
   end
