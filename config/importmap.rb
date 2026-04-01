6a3650408a5f238e7cba2684625b7f2dd647a454
Miya096jp <miyawaki.yske@gmail.com>
2026-04-01 14:19:20 +0900

    Config: dexieを追加しindexedDBのstoreを作成

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "dexie", to: "https://cdn.jsdelivr.net/npm/dexie@4.0.11/dist/dexie.mjs"
+pin "process" # @2.1.0
+pin "lib/database", to: "lib/database.js"
