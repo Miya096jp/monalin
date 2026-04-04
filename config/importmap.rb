f0ad4901dc10375a9fb14e44d0fc123a9bde8f13
Miya096jp <miyawaki.yske@gmail.com>
2026-04-05 07:09:21 +0900

    Test: 写真撮影画面の開閉のUIについてsystem spec追加
    video_capture_specの既存のインターバル撮影のテストを削除
    (理由: getUserMediaがヘッドレス環境で動作せず)
    
    Dexie.jsをテスト環境で読み込めむためにCDN依存からjavascript/vendorにインストール

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "dexie", to: "https://cdn.jsdelivr.net/npm/dexie@4.0.11/dist/dexie.mjs"
+# pin "dexie", to: "https://cdn.jsdelivr.net/npm/dexie@4.0.11/dist/dexie.mjs"
+pin "dexie", to: "dexie.js", preload: true
 pin "process" # @2.1.0
 pin "lib/database", to: "lib/database.js"
