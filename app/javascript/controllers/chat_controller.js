2e1aa18b644e3a0d927633de9c4a9f022013fe55
Miya096jp <miyawaki.yske@gmail.com>
2026-04-28 17:54:55 +0900

    Feature: DOMに追加された最新の要素に自動でスクロールする

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -3,6 +3,37 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
 	static targets = ["loader", "container"];
 
+	connect() {
+		this.startObserve();
+	}
+
+	startObserve() {
+		const config = {
+			childList: true,
+			attributes: false,
+			subtree: true,
+		};
+
+		const callback = (mutationList, observer) => {
+			const hasAddedNode = mutationList.some((mutationRecord) => {
+				return (
+					mutationRecord.type === "childList" &&
+					mutationRecord.addedNodes.length > 0
+				);
+			});
+
+			if (hasAddedNode) {
+				this.containerTarget.lastElementChild.scrollIntoView({
+					behavior: "smooth",
+					block: "end",
+				});
+			}
+		};
+
+		this.observer = new MutationObserver(callback);
+		this.observer.observe(this.containerTarget, config);
+	}
+
 	showLoader() {
 		this.containerTarget.appendChild(this.loaderTarget);
 		this.loaderTarget.classList.remove("hidden");
@@ -13,5 +44,9 @@ export default class extends Controller {
 		if (stream.getAttribute("target") === "chat") {
 			this.loaderTarget.classList.add("hidden");
 		}
+	}
+
+	disconnect() {
+		this.observer.disconnect();
 	}
 }
